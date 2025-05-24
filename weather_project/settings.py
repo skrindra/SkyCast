5bd58ebebc210bbdc032f82146a40b2f182db62d
Shiva <122610403+skrindra@users.noreply.github.com>
2025-05-25 02:54:43 +0530

    Update settings.py

--- weather_project/settings.py
+++ weather_project/settings.py
@@ -133,7 +133,8 @@ if not DEBUG:
 
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
